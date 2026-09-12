102da8b43bab3d36dfcc4151dcad9c0ec4fd5cbe
raushan_kumar_yadav <raushankumar911316@gmail.com>
2026-09-12 15:21:47 +0530

    finished create project widget component

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -17,6 +17,9 @@ const createWindow = () => {
     },
   });
 
+   const backendPort = 8080; 
+
+  mainWindow.webContents.executeJavaScript(`window.__ECHO_PORT__ = ${backendPort}`);
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
